43e6d58c3a6e89867cb43005da93ef0534dfb883
Chris Nicola <chris.nicola@wealthbar.com>
2017-07-05 13:47:36 -0700

    Ensure Node.js promise exceptions are reported in tests

--- util/test_helper.js
+++ util/test_helper.js
@@ -1,2 +1,5 @@
 require('browser-env')();
 require('vue').config.productionTip = false;
+
+// Node 8 prints a warning and catches unhandled promise rejections.
+process.on('unhandledRejection', (e) => { throw e; });
