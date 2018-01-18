d1a1dd9b319095c43863a550a6e515d64bf211e7
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2018-01-18 15:17:29 -0800

    Typography Init

--- src/app.vue
+++ src/app.vue
@@ -1,49 +1,173 @@
 <template>
   <body>
-    <banner @menu="toggleMenu"></banner>
-    <side-menu :opened="menuDisplay" @close="toggleMenu">
-      <ul slot="content"  class="side-menu-content">
-        <li><router-link @click.native="toggleMenu" to="/">Typography</router-link></li>
-        <li><router-link @click.native="toggleMenu" to="/components/icons">Icons</router-link></li>
-        <li><router-link @click.native="toggleMenu" to="/components/loginexample">Login</router-link></li>
-        <li><router-link @click.native="toggleMenu" to="/components/modalexample">Modal</router-link></li>
-        <li><router-link @click.native="toggleMenu" to="/components/contextmenuexample">Context Menu</router-link></li>
-        <li><router-link @click.native="toggleMenu" to="/components/sidemenuexample">Side Menu</router-link></li>
-        <li><router-link @click.native="toggleMenu" to="/components/toggleswitchexample">Toggle switch</router-link></li>
-       <!-- <li><router-link :to="example.path">{{example.name}}</router-link></li>-->
-      </ul>
-    </side-menu>
-    <login v-if="isLoginDisplayed"></login>
-    <router-view id="app" v-if="!isLoginDisplayed"></router-view>
+    <main class="home">
+    <article>
+      <img class="logo" src="~@//assets/logo.svg">
+
+      <h1>WealthBar Vue Starter Kit</h1>
+      <h2>This is a title header</h2>
+      <h3>This is a subheader</h3>
+      <h4>This is a minor header</h4>
+      <h5>This is a large body copy</h5>
+      <hr/>
+      <p>
+        This is basic body copy</br>
+        <strong>This is bold body copy</strong></br>
+        <em>This is subtle body copy</em></br>
+        <small>This is small body copy</small>
+      </p>
+      <hr/>
+      <section>
+        <p class="one-three">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo condimentum odio in auctor. Integer consectetur augue purus, eget gravida eros tincidunt ut. Maecenas efficitur laoreet turpis in sodales. Aenean porta ligula magna, sed vestibulum odio facilisis quis. Donec vitae tincidunt nibh, at laoreet purus. Curabitur blandit sit amet tortor eget blandit. Vestibulum eu convallis diam. Curabitur a ultrices elit, in placerat lectus. Fusce fringilla, quam ut rutrum posuere, neque purus venenatis justo, et vestibulum nisi lacus vel ipsum. Fusce leo est, iaculis vitae efficitur vel, dictum ut lacus. Ut dignissim nec massa laoreet lacinia. Donec lacinia porta risus vitae tempor. Duis tincidunt nibh diam, eu mattis libero sagittis ac.
+        </p>
+        <p class="one-four">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo condimentum odio in auctor. Integer consectetur augue purus, eget gravida eros tincidunt ut. Maecenas efficitur laoreet turpis in sodales. Aenean porta ligula magna, sed vestibulum odio facilisis quis. Donec vitae tincidunt nibh, at laoreet purus. Curabitur blandit sit amet tortor eget blandit. Vestibulum eu convallis diam. Curabitur a ultrices elit, in placerat lectus. Fusce fringilla, quam ut rutrum posuere, neque purus venenatis justo, et vestibulum nisi lacus vel ipsum. Fusce leo est, iaculis vitae efficitur vel, dictum ut lacus. Ut dignissim nec massa laoreet lacinia. Donec lacinia porta risus vitae tempor. Duis tincidunt nibh diam, eu mattis libero sagittis ac.
+        </p>
+        <p class="one-five">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo condimentum odio in auctor. Integer consectetur augue purus, eget gravida eros tincidunt ut. Maecenas efficitur laoreet turpis in sodales. Aenean porta ligula magna, sed vestibulum odio facilisis quis. Donec vitae tincidunt nibh, at laoreet purus. Curabitur blandit sit amet tortor eget blandit. Vestibulum eu convallis diam. Curabitur a ultrices elit, in placerat lectus. Fusce fringilla, quam ut rutrum posuere, neque purus venenatis justo, et vestibulum nisi lacus vel ipsum. Fusce leo est, iaculis vitae efficitur vel, dictum ut lacus. Ut dignissim nec massa laoreet lacinia. Donec lacinia porta risus vitae tempor. Duis tincidunt nibh diam, eu mattis libero sagittis ac.
+        </p>
+      </section>
+      <hr/>
+      <section>
+        <p class="one-three">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo condimentum odio in auctor.
+        </p>
+        <p class="one-four">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <p class="one-five">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo condimentum odio in auctor.
+        </p>
+      </section>
+      <hr/>
+        <section>
+          <h1>Privacy Policy</h1>
+        </section>
+        <section>
+          <div>
+            <p>At Wealthbar Financial Services Inc ("WealthBar") we are concerned with safeguarding and protecting the privacy of your personal information. In accordance with Canada's *Personal Information Protection & Electronic Documents Act* ("PIPEDA") we have established policies on how we collect, use and protect any information that you give us when you use our website or services.</p>
+          </div>
+          <div>
+            <h2>What We Collect</h2>
+
+            <h3>Required Regulatory Information</h3>
+
+            <p>WealthBar collects information needed to provide services and meet the regulatory requirements of our industry. When you open an account or sign up for services, we collect personal information which may include but is not limited to the following:</p>
+
+            <ul>
+              <li>Your name and date of birth</li>
+              <li>Contact details</li>
+              <li>Nature of your employment or business</li>
+              <li>Investment needs and objectives</li>
+              <li>Financial circumstances</li>
+              <li>Risk tolerance</li>
+              <li>Demographics</li>
+              <li>Other information relevant to the services we provide</li>
+            </ul>
+          </div>
+
+          <div>
+            <h3>Passive Data Collection</h3>
+
+            <p>We may receive passively-collected information through a variety of methods, including “cookies” to collect information.</p>
+          </div>
+
+          <div>
+            <h4>What is a cookie?</h4>
+
+            <p>Cookies are text files containing small amounts of information which are downloaded to your device when you visit a website. Cookies are then sent back to the originating website on each subsequent visit, or to another website that recognizes that cookie. <a href="http://www.allaboutcookies.org/">You can get more information about cookies here.</a></p>
+
+            <p>WealthBar uses cookies to track and measure site performance and gather analytics on usage. This information is anonymous and is only used by WealthBar to measure and improve our site.</p>
+
+            <p>When sending e-mail we may use tracking pixels and urls to help us measure open rates for e-mails and link clicks. This tracking information is used internally only to help us provide relevant content and is never shared with any third parties.</p>
+          </div>
+
+          <div>
+            <h4>Opting Out of Cookies</h4>
+
+            <p>If you choose, you may set your browser to notify you when you receive a cookie, giving you the chance to decide whether or not to accept it.</p>
+          </div>
+
+          <div>
+            <h4>Analytics And Marketing</h4>
+
+            <p>We use analytics tools and other third-party technologies, such as Google Analytics, to collect non-personal information in the form of various usage and user metrics when you use our online Sites and/or Services.</p>
+
+            <p>We also use online advertising to reach new potential clients and remarketing tools that allow us to target ads to specific groups of people who have previously interacted with WealthBar’s websites.</p>
+
+            <p>Google allows individuals to opt out of it’s advertising cookies from its <a href="http://www.google.com/settings/ads">ad settings page.</a></p>
+
+            <p>Google has additional information available about their <a href="http://adwords.google.com/support/aw/bin/answer.py?hl=en&answer=100746">Remarketing Privacy Guidelines</a>, and <a href="https://support.google.com/adwords/answer/2549116?hl=en">Restrictions.</a></p>
+          </div>
+
+          <div>
+
+            <h3>Where do we store it</h3>
+
+            <p>WealthBar may use third parties to store and process data. WealthBar will ensure that su-h third-party service providers have significant expertise with data and digital privacy.</p>
+
+            <p> WealthBar will ensure that personal data is stored and transmitted in encrypted format using technology such as SSL. Third party data storage and processing providers may be located in the United States and as a result personal information may be subject to U.S. privacy law which may differ from Canadian privacy law.</p>
+          </div>
+
+          <div>
+            <h3>How We Use It</h3>
+
+            <p>The information we collect is used so we can communicate with you, provide wealth management services, improve our products and services, and meet our regulatory obligations.</p>
+
+            <p>We share only the information which is reasonably required in the normal course of business with third parties such as our custodians, securities regulators, auditors, legal counsel, or law enforcement agencies, to enable them to fulfill their obligations to WealthBar and our clients.</p>
+
+            <p> We may also disclose your personal information to third-party service providers, affiliates and agents in order to assist us in providing the services you requested and to fulfill the purposes for which your personal information has been collected.</p>
+
+            <p>Outside of this we shall not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so.</p>
+          </div>
+
+          <div>
+            <h3>How We Protect It</h3>
+
+            <p>We are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online. In the event that we share your personal information with third-party service providers, affiliates and agents, these parties will be required to adhere to strict confidentiality obligations and practices.</p>
+
+            <p>If you have a question, concern or complaint about our privacy policy, please send your comments and your contact details to our Privacy Officer in writing to the following address:</p>
+
+            <p>WealthBar Financial Services Inc. 490 - 1122 Mainland St. Vancouver, BC V6B 5L1 Attn: Privacy Officer <a href="mailto:info@wealthbar.com">info@wealthbar.com</a></p>
+
+            <p>If you believe that any information we have on file for you is incorrect or incomplete, please write to or email us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.</p>
+          </div>
+          <button>Button</button>
+        </section>
+    </article>
+    <footer>
+
+    </footer>
+  </main>
   </body>
 </template>
 
 <script>
-import { banner, login, sideMenu } from '@/components';
-import { mapGetters } from '@/lib/vue';
 
 export default {
   name: 'app',
-  computed: {
-    ...mapGetters(['isLoggedIn']),
-    isLoginDisplayed() {
-      return this.$route.meta.requiresAuth === true && this.isLoggedIn === false;
-    },
-  },
-  components: { banner, login, sideMenu },
-  data() {
-    return {
-      menuDisplay: false,
-    };
-  },
-  methods: {
-    toggleMenu() {
-      this.menuDisplay = !this.menuDisplay;
-    },
-  },
 };
 </script>
 
 <style lang="scss">
   @import 'style.scss';
+
+  main {
+    max-width: 36rem;
+    margin: auto;
+  }
+
+  .one-three {
+    line-height: 1.4;
+  }
+
+  .one-four {
+    line-height: 1.45;
+  }
+
+  .one-five {
+    line-height: 1.5;
+  }
+
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,25 +1,5 @@
 <template>
-  <main class="home">
-    <article>
-      <img class="logo" src="~@//assets/logo.svg"> 
 
-      <h1>WealthBar Vue Starter Kit</h1>
-      <h2>This is a title header</h2>
-      <h3>This is a subheader</h3>
-      <h4>This is a minor header</h4>
-      <h5>This is a large body copy</h5>
-      <hr/>
-      <p>
-        This is basic body copy</br>
-        <strong>This is bold body copy</strong></br>
-        <em>This is subtle body copy</em></br>
-        <small>This is small body copy</small>
-      </p>
-    </article>
-    <footer>
-      <router-link to="/components">Components</router-link>
-    </footer>
-  </main>
 </template>
 
 <script>
