6aac04ef7d8ec0db5e655ef925f83719455071dd
Sharifa Samuels <sharifa.samuels@wealthbar.com>
2018-03-14 14:00:04 -0700

    clean up alignment of code

--- src/app.vue
+++ src/app.vue
@@ -2,59 +2,38 @@
   <body>
     <main class="home">
       <side-menu :opened="menuDisplayed" >
-      <ul slot="content" class="side-menu-content">
-      </ul>
-    </side-menu>
-    <article>
-      <img class="logo" src="~@//assets/logo.svg">
-
-      <h1>WealthBar Vue Starter Kit</h1>
-      <h2>This is a title header</h2>
-      <h3>This is a subheader</h3>
-      <h4>This is a minor header</h4>
-      <h5>This is a large body copy</h5>
-      <hr/>
-      <p>
-        This is basic body copy</br>
-        <strong>This is bold body copy</strong></br>
-        <em>This is subtle body copy</em></br>
-        <small>This is small body copy</small>
-      </p>
-      <hr/>
-
-<section>
-      <h2>Font Stack</h2>
-      <hr/>
-      <h3>headers</h3>
-      <pre>
-        <code>
-"GT-Walsheim-Bold", sans-serif !default;
-</code>
-</pre>
-      <h3>body/copy</h3>
-      <pre>
-        <code>
-"Avenir", "Helvetica Neue", Helvetica, Arial, sans-serif !default;
-</code>
-</pre>
-</section>
-
-<section>
-      <h2>Headers</h2>
-      <hr/>
-      <h1>H1 Header Element</h1>
-      <pre>
-        <code>
-h1 {
-  font-size: 1.875rem;
-  line-height: 1.375rem;
-  font-weight: 600; //GT Walsheim Medium
-}
-</code>
-</pre>
-</section>
-     </article>
-  </main>
+        <ul slot="content" class="side-menu-content"></ul>
+      </side-menu>
+      <article>
+        <img class="logo" src="~@//assets/logo.svg">
+      <section>
+        <h2>Font Stack</h2>
+        <hr/>
+        <h3>headers</h3>
+        <pre>
+          <code>"GT-Walsheim-Bold", sans-serif !default;</code>
+        </pre>
+        <h3>body/copy</h3>
+          <pre>
+            <code>"Avenir", "Helvetica Neue", Helvetica, Arial, sans-serif !default;</code>
+          </pre>
+      </section>
+      <section>
+        <h2>Headers</h2>
+        <hr/>
+        <h1>H1 Header Element</h1>
+        <pre>
+          <code>
+            h1 {
+              font-size: 1.875rem;
+              line-height: 1.375rem;
+              font-weight: 600; //GT Walsheim Medium
+            }
+          </code>
+        </pre>
+      </section>
+      </article>
+    </main>
   </body>
 </template>
 
@@ -65,7 +44,7 @@ h1 {
     components: { sideMenu },
     data() {
       return {
-        menuDisplayed: false,
+        menuDisplayed: true,
       };
     },
   };
