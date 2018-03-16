9268711976a26f8a93fc7bb13a0649809c9ebca6
Sharifa Samuels <sharifa.samuels@wealthbar.com>
2018-03-16 13:38:39 -0700

    locate nav area

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <ul>
-      <li><router-link to="/typography">Type</router-link></li>
-    </ul>
-  <router-view></router-view>
+  <div class="app-view">
+    <nav>
+      <ul>
+        <li><router-link to="/typography">Type</router-link></li>
+      </ul>
+    </nav>
+    <section>
+      <router-view></router-view>
+    </section>
   </div>
 </template>
 
