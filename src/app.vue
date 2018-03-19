e798d5e789344e60d6e4c097c693de507d1df80f
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2018-03-19 14:22:42 -0700

    GT-walsheim font update, minor layout and typographical changes, updated typography page

--- src/app.vue
+++ src/app.vue
@@ -20,45 +20,4 @@
 
 <style lang="scss">
   @import 'style.scss';
-
-  main {
-    margin: auto;
-  }
-
-  .logo {
-    margin: 2rem 4rem;
-  }
-
-  .logo-area {
-    display: flex;
-    justify-content: center;
-  }
-
-  section.thirds {
-    display: flex;
-    width: 100%;
-    align-items: flex-start;
-    flex-direction: column;
-
-    @media #{$sw-medium} {
-      flex-direction: row;
-    }
-
-    > div {
-      flex: 1 0 33%;
-      padding: 0 1rem;
-    }
-  }
-
-  p {
-    font-size: 1rem;
-    line-height: 1.25rem;
-    &.one-three-eighths { line-height: 1.375rem; }
-    &.one-seven-sixteenths { line-height: 1.4375rem; }
-    &.one-half { line-height: 1.5rem; }
-    &.one-five-eights { line-height: 1.625rem; }
-    &.large { font-size: 1.125rem; }
-    span { color: $brand; }
-  }
-
 </style>
--- src/views/typography.vue
+++ src/views/typography.vue
@@ -8,7 +8,7 @@
 
       <p>[ mobile | desktop ]</p>
 
-      <h1>H1 This is a huge header.</h1>
+      <h1>H1 This is a <span class="large">huge</span> huge header.</h1>
       <pre><code>&lt;h1&gt;H1 This is a huge header.&lt;/h1&gt;</code></pre>
 
       <h2>H2 This is a big header.</h2>
@@ -27,11 +27,36 @@
       <h3>Base style</h3>
       <p>Our base paragraph style is the foundation of our typography. All styles are variants of this style. It is also the default text style, meaning if no style is applied or specified, it should use this style.</p>
       <p>This is a paragraph.</p>
+      <div class="code-split">
       <pre><code>&lt;p&gt;This is a paragraph.&lt;/p&gt;</code></pre>
+      <p><pre>font-size: 16px
+line-height: 22px
+font-weight: normal
+padding: 16px 0px 16px</pre></p>
+      </div>
       <h2>Additional Styles</h2>
+      <p class="small">This is a small text paragraph.</small></p>
+      <pre><code>&lt;p class="small"&gt;This is small text.&lt;/p&gt;</code></pre>
+      <p class="large">This is a large text paragraph.</p>
+      <pre><code>&lt;p class=”large”&gt;This is large text.&lt;/p&gt;</code></pre>
+      <p>This is <span class="subtle">subtle text</span> used in a paragraph.</p>
+      <pre><code>&lt;p&gt;This is &lt;span class="subtle"&gt;subtle text&lt;/span&gt; used in a paragraph.&lt;/p&gt;</code></pre>
+      <p>This is <strong>strong text</strong> used in a paragraph.</p>
+      <pre><code>&lt;p&gt;This is &lt;strong&gt;strong text&lt;/strong&gt; used in a paragraph.&lt;/p&gt;</code></pre>
     </section>
-      <h3>body/copy</h3>
+    <section>
+      <h2>Links</h2>
+      <p>This is our default style for text links within paragraphs. Links inherit the paragraph’s size and style. We use progressive enhancement with a basic fallback if browsers don’t support certain types of styling.</p>
+      <p><a href="#">This is a link</a></p>
+      <p><button>Button link</button></p>
+      <pre><code>&lt;a href="http://wealthbar.com"&gt;This is a link&lt;/a&gt;
+&lt;button&gt;button link&lt;/button&gt;</code></pre>
+    </section>
+    <section>
+      <h2>Font Stack</h2>
+      <p>We use the same font stack regardless of the device. GT Walsheim is our typeface for it’s friendly and unique personality, as well as its web readability.</p>
       <pre><code>"GT-Walsheim", "Avenir", "Helvetica Neue", Helvetica, Arial, sans-serif;</code></pre>
+    </section>
   </article>
 </template>
 
@@ -41,5 +66,4 @@
 </script>
 
 <style lang="scss">
-  @import 'style.scss';
 </style>
