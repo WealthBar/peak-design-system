e3d01be899a4ae6d65fe102eef5ffac991cf91d0
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2018-09-20 11:01:35 -0700

    remove unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,26 +1,8 @@
-import modal from './modal';
-import contextMenu from './context_menu';
-import login from './login';
-import banner from './banner';
-import sideMenu from './side_menu';
-import toggleSwitch from './toggle_switch';
-import list from './list';
-import profile from './profile';
-import password from './password';
 import toggleView from './toggle_view';
 import BackToTop from './back_to_top';
 
 /* eslint-disable import/prefer-default-export */
 export {
-  modal,
-  contextMenu,
-  login,
-  banner,
-  sideMenu,
-  toggleSwitch,
-  list,
-  profile,
-  password,
   toggleView,
   BackToTop,
 };
