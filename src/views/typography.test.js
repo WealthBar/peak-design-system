0b0cafbf962ca743d74cc343be39c116e8c5feb6
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2018-09-20 11:05:30 -0700

    remove page test

--- src/views/typography.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'avoriaz';
-import test from 'tape';
-import typography from './typography';
-
-test('views/typography data', (t) => {
-  const wrapper = shallow(typography);
-  t.equal(wrapper.data().title, 'Typography', 'title set on page');
-  t.end();
-});
