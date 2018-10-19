6157ad8d501fba4c9e19416c1efbb2cbb58a42b9
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2018-10-19 10:20:56 -0700

    naming convention updates

--- src/views/buttons.vue
+++ src/views/buttons.vue
@@ -2,20 +2,20 @@
   <article class="container">
     <h1>Buttons</h1>
     <section>
-      <h3>Default Buttons</h3>
+      <h3>Primary Buttons</h3>
       <div class="attributes">
         <strong>Example</strong>
         <div class="example">
-          <button>No Type</button>
-          <button type="button">Type Button</button>
+          <button>Unstyled</button>
+          <button type="button">Fill</button>
           <button type="button" class="outline">Outline</button>
           <button type="button" class="flat">Flat</button>
           <button type="button" class="subtle">Subtle</button>
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button&gt;No Type&lt;/button&gt;
-          &lt;button type="button"&gt;Type Button&lt;/button&gt;
+          <pre><code class="html">&lt;button&gt;Unstyled&lt;/button&gt;
+          &lt;button type="button"&gt;Fill&lt;/button&gt;
           &lt;button type="button" class="outline"&gt;Outline&lt;/button&gt;
           &lt;button type="button" class="flat"&gt;Flat&lt;/button&gt;
           &lt;button type="button" class="subtle"&gt;Subtle&lt;/button&gt;
@@ -30,16 +30,16 @@
       <div class="attributes">
         <strong>Example</strong>
         <div class="example">
-          <button class="alt">No Type</button>
-          <button type="button" class="alt">Type Button</button>
+          <button class="alt">Unstyled</button>
+          <button type="button" class="alt">Fill</button>
           <button type="button" class="outline alt">Outline</button>
           <button type="button" class="flat alt">Flat</button>
           <button type="button" class="subtle alt">Subtle</button>
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="alt"&gt;No Type&lt;/button&gt;
-          &lt;button type="button" class="alt"&gt;Type Button&lt;/button&gt;
+          <pre><code class="html">&lt;button class="alt"&gt;Unstyled&lt;/button&gt;
+          &lt;button type="button" class="alt"&gt;Fill&lt;/button&gt;
           &lt;button type="button" class="outline alt"&gt;Outline&lt;/button&gt;
           &lt;button type="button" class="flat alt"&gt;Flat&lt;/button&gt;
           &lt;button type="button" class="subtle alt"&gt;Subtle&lt;/button&gt;
@@ -54,16 +54,16 @@
       <div class="attributes">
         <strong>Example</strong>
         <div class="example">
-          <button class="alert">No Type</button>
-          <button type="button" class="alert">Type Button</button>
+          <button class="alert">Unstyled</button>
+          <button type="button" class="alert">Fill</button>
           <button type="button" class="outline alert">Outline</button>
           <button type="button" class="flat alert">Flat</button>
           <button type="button" class="subtle alert">Subtle</button>
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="alert"&gt;No Type&lt;/button&gt;
-          &lt;button type="button" class="alert"&gt;Type Button&lt;/button&gt;
+          <pre><code class="html">&lt;button class="alert"&gt;Unstyled&lt;/button&gt;
+          &lt;button type="button" class="alert"&gt;Fill&lt;/button&gt;
           &lt;button type="button" class="outline alert"&gt;Outline&lt;/button&gt;
           &lt;button type="button" class="flat alert"&gt;Flat&lt;/button&gt;
           &lt;button type="button" class="subtle alert"&gt;Subtle&lt;/button&gt;
@@ -78,16 +78,16 @@
       <div class="attributes">
         <strong>Example</strong>
         <div class="example">
-          <button class="neg">No Type</button>
-          <button type="button" class="neg">Type Button</button>
+          <button class="neg">Unstyled</button>
+          <button type="button" class="neg">Fill</button>
           <button type="button" class="outline neg">Outline</button>
           <button type="button" class="flat neg">Flat</button>
           <button type="button" class="subtle neg">Subtle</button>
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="neg"&gt;No Type&lt;/button&gt;
-          &lt;button type="button" class="neg"&gt;Type Button&lt;/button&gt;
+          <pre><code class="html">&lt;button class="neg"&gt;Unstyled&lt;/button&gt;
+          &lt;button type="button" class="neg"&gt;Fill&lt;/button&gt;
           &lt;button type="button" class="outline neg"&gt;Outline&lt;/button&gt;
           &lt;button type="button" class="flat neg"&gt;Flat&lt;/button&gt;
           &lt;button type="button" class="subtle neg"&gt;Subtle&lt;/button&gt;
@@ -102,16 +102,16 @@
       <div class="attributes">
         <strong>Example</strong>
         <div class="example">
-          <button class="pos">No Type</button>
-          <button type="button" class="pos">Type Button</button>
+          <button class="pos">Unstyled</button>
+          <button type="button" class="pos">Fill</button>
           <button type="button" class="outline pos">Outline</button>
           <button type="button" class="flat pos">Flat</button>
           <button type="button" class="subtle pos">Subtle</button>
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="pos"&gt;No Type&lt;/button&gt;
-          &lt;button type="button" class="pos"&gt;Type Button&lt;/button&gt;
+          <pre><code class="html">&lt;button class="pos"&gt;Unstyled&lt;/button&gt;
+          &lt;button type="button" class="pos"&gt;Fill&lt;/button&gt;
           &lt;button type="button" class="outline pos"&gt;Outline&lt;/button&gt;
           &lt;button type="button" class="flat pos"&gt;Flat&lt;/button&gt;
           &lt;button type="button" class="subtle pos"&gt;Subtle&lt;/button&gt;
@@ -155,8 +155,8 @@
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="icon"&gt;No Type&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
-          &lt;button type="button" class="icon after"&gt;Type Button&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
+          <pre><code class="html">&lt;button class="icon"&gt;Unstyled&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
+          &lt;button type="button" class="icon after"&gt;Fill&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="outline"&gt;Outline&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="flat"&gt;Flat&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="subtle"&gt;Subtle&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
@@ -167,13 +167,13 @@
         <strong>Example</strong>
         <div class="example">
           <button class="icon after">
-            No Type
+            Unstyled
             <svg width="16" height="16" viewBox="0 0 16 16">
               <path d="M6.426 0l-.89.89 6.48 6.481H0V8.63h12.016l-6.48 6.48.89.89 8-8z"/>
             </svg>
           </button>
           <button type="button" class="icon after">
-            Type Button
+            Fill
             <svg width="16" height="16" viewBox="0 0 16 16">
               <path d="M6.426 0l-.89.89 6.48 6.481H0V8.63h12.016l-6.48 6.48.89.89 8-8z"/>
             </svg>
@@ -199,8 +199,8 @@
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="icon after"&gt;No Type&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
-          &lt;button type="button" class="icon after"&gt;Type Button&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
+          <pre><code class="html">&lt;button class="icon after"&gt;Unstyled&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
+          &lt;button type="button" class="icon after"&gt;Fill&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="outline icon after"&gt;Outline&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="flat icon after"&gt;Flat&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="subtle icon after"&gt;Subtle&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
@@ -210,17 +210,17 @@
         <h4>Icon Before</h4>
         <strong>Example</strong>
         <div class="example">
-          <button class="icon before">
+          <button>
             <svg width="16" height="16" viewBox="0 0 16 16">
               <path d="M6.426 0l-.89.89 6.48 6.481H0V8.63h12.016l-6.48 6.48.89.89 8-8z"/>
             </svg>
-            No Type
+            Unstyled
           </button>
           <button type="button" class="icon before">
             <svg width="16" height="16" viewBox="0 0 16 16">
               <path d="M6.426 0l-.89.89 6.48 6.481H0V8.63h12.016l-6.48 6.48.89.89 8-8z"/>
             </svg>
-            Type Button
+            Fill
           </button>
           <button type="button" class="outline icon before">
             <svg width="16" height="16" viewBox="0 0 16 16">
@@ -243,8 +243,8 @@
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="icon before"&gt;No Type&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
-          &lt;button type="button" class="icon before"&gt;Type Button&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
+          <pre><code class="html">&lt;button class="icon before"&gt;Unstyled&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
+          &lt;button type="button" class="icon before"&gt;Fill&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="outline icon before"&gt;Outline&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="flat icon before"&gt;Flat&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
           &lt;button type="button" class="subtle icon before"&gt;Subtle&lt;svg&gt;&hellip;&lt;svg&gt;&lt;/button&gt;
@@ -258,12 +258,12 @@
       <div class="attributes">
         <strong>Example</strong>
         <div class="example">
-          <button class="large">No Type</button>
-          <button type="button" class="large icon after">Type Button</button>
+          <button class="large">Unstyled</button>
+          <button type="button" class="large icon after">Fill</button>
           <button type="button" class="outline large">Outline</button>
           <button type="button" class="flat large">Flat</button>
           <button type="button" class="subtle large">Subtle</button>
-          <button type="button" class="large icon after">Type Button
+          <button type="button" class="large icon after">Fill
             <svg width="16" height="16" viewBox="0 0 16 16">
               <path d="M6.426 0l-.89.89 6.48 6.481H0V8.63h12.016l-6.48 6.48.89.89 8-8z"/>
             </svg>
@@ -276,8 +276,8 @@
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="large"&gt;No Type&lt;/button&gt;
-          &lt;button type="button" class="large"&gt;Type Button&lt;/button&gt;
+          <pre><code class="html">&lt;button class="large"&gt;Unstyled&lt;/button&gt;
+          &lt;button type="button" class="large"&gt;Fill&lt;/button&gt;
           &lt;button type="button" class="outline large"&gt;Outline&lt;/button&gt;
           &lt;button type="button" class="flat large"&gt;Flat&lt;/button&gt;
           &lt;button type="button" class="subtle large"&gt;Subtle&lt;/button&gt;
@@ -291,12 +291,12 @@
       <div class="attributes">
         <strong>Example</strong>
         <div class="example">
-          <button class="small">No Type</button>
-          <button type="button" class="small">Type Button</button>
+          <button class="small">Unstyled</button>
+          <button type="button" class="small">Fill</button>
           <button type="button" class="outline small">Outline</button>
           <button type="button" class="flat small">Flat</button>
           <button type="button" class="subtle small">Subtle</button>
-          <button type="button" class="small icon after">Type Button
+          <button type="button" class="small icon after">Fill
             <svg width="16" height="16" viewBox="0 0 16 16">
               <path d="M6.426 0l-.89.89 6.48 6.481H0V8.63h12.016l-6.48 6.48.89.89 8-8z"/>
             </svg>
@@ -309,8 +309,8 @@
         </div>
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="html">&lt;button class="small"&gt;No Type&lt;/button&gt;
-          &lt;button type="button" class="small"&gt;Type Button&lt;/button&gt;
+          <pre><code class="html">&lt;button class="small"&gt;Unstyled&lt;/button&gt;
+          &lt;button type="button" class="small"&gt;Fill&lt;/button&gt;
           &lt;button type="button" class="outline small"&gt;Outline&lt;/button&gt;
           &lt;button type="button" class="flat small"&gt;Flat&lt;/button&gt;
           &lt;button type="button" class="subtle small"&gt;Subtle&lt;/button&gt;
