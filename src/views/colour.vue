e613ca85ed08eeca0b659062f71cc63cdadba1c5
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2019-04-25 17:13:02 -0700

    Colour Theme switching

--- src/views/colour.vue
+++ src/views/colour.vue
@@ -8,218 +8,27 @@
     <hr>
 
     <h3>Colour Weight</h3>
-    <p>In an effort to bulletproof our system of colours, we use weights from 50 to 900, similar to font weights. Weight is defined by the contrast ratio of the colour against white, allowing us to convey relevant weights across our entire colour palette. This means $emerald-50 and $ruby-50 should be pretty close in weight, even if they’re on opposite ends of the colour spectrum.</p>
-
-    <h3>Brand</h3>
-    <p>These colours are used across marketing, product, and beyond. These colours define our brand and play a large part in the visual identity of WealthBar.</p>
-    <div class="colour-block dark" style="background: #00B298;">
-      <strong>$jade</strong>
-    </div>
-    <div class="color-pallete">
-      <div class="colour-block" style="background: #ecf9f5;">
-        <strong>$jade-50</strong>
-        <span>#ECF9F5</span>
-      </div>
-      <div class="colour-block dark" style="background: #68ccb1;">
-        <strong>$jade-100</strong>
-        <span>#68CCB1</span>
-      </div>
-      <div class="colour-block dark" style="background: #00B298;">
-        <strong>$jade-300</strong>
-        <span>#00B298</span>
-      </div>
-      <div class="colour-block dark" style="background: #008579;">
-        <strong>$jade-500</strong>
-        <span>#008579</span>
-      </div>
-      <div class="colour-block dark" style="background: #0e6b64;">
-        <strong>$jade-700</strong>
-        <span>#0E6B64</span>
-      </div>
-      <div class="colour-block dark" style="background: #10504E;">
-        <strong>$jade-900</strong>
-        <span>#10504E</span>
-      </div>
-
-    </div>
-
-    <br>
-
-    <div class="colour-block dark" style="background: #787A97;">
-      <strong>$amethyst</strong>
-    </div>
-    <div class="color-pallete">
-      <div class="colour-block" style="background: #e8e9f1;">
-        <strong>$amethyst-50</strong>
-        <span>#E8E9F1</span>
-      </div>
-      <div class="colour-block dark" style="background: #9d9fbc;">
-        <strong>$amethyst-100</strong>
-        <span>#9D9FBC</span>
-      </div>
-      <div class="colour-block dark" style="background: #787A97;">
-        <strong>$amethyst-300</strong>
-        <span>#787A97</span>
-      </div>
-      <div class="colour-block dark" style="background: #5d5f7a;">
-        <strong>$amethyst-500</strong>
-        <span>#5D5F7A</span>
-      </div>
-      <div class="colour-block dark" style="background: #464855;">
-        <strong>$amethyst-700</strong>
-        <span>#464855</span>
-      </div>
-    </div>
-
-    <br>
-
-    <div class="colour-block" style="background: #F5BE44;">
-      <strong>$amber</strong>
-    </div>
-    <div class="color-pallete">
-      <div class="colour-block" style="background: #fee5a0;">
-        <strong>$amber-50</strong>
-        <span>#FEE5A0</span>
-      </div>
-      <div class="colour-block" style="background: #F5BE44;">
-        <strong>$amber-100</strong>
-        <span>#F5BE44</span>
-      </div>
-      <div class="colour-block" style="background: #da9f18;">
-        <strong>$amber-300</strong>
-        <span>#DA9F18</span>
-      </div>
-    </div>
-
-    <h4>When to choose between Jade & Mint:</h4>
-    <p>$jade-50 and $mint-50 are very similar, but are used for different purposes. $jade-50 is brighter and used in smaller doses, primarily to highlight hover or selected effects in specific UI components, such as a hover effect in a context menu item. $mint-50 is used in marketing; it is more muted, so it is ideal for larger backgrounds, such as the footer in the brochure site.</p>
-
-    <div class="color-pallete">
-      <div class="colour-block" style="background: #ebf4f1;">
-        <strong>$mint-50</strong>
-        <span>#EBF4F1</span>
-      </div>
-      <div class="colour-block" style="background: #d2e6de;">
-        <strong>$mint-100</strong>
-        <span>#D2E6DE</span>
-      </div>
-    </div>
-
-    <h3>UI</h3>
-    <p>These colours are primarily used for functional purposes in our product interface.</p>
-
-    <div class="color-pallete">
-      <div class="colour-block" style="background: #fcefed;">
-        <strong>$ruby-50</strong>
-        <span>#FCEFED</span>
-      </div>
-      <div class="colour-block dark" style="background: #e96654;">
-        <strong>$ruby-300</strong>
-        <span>#E96654</span>
-      </div>
-      <div class="colour-block dark" style="background: #d93939;">
-        <strong>$ruby-500</strong>
-        <span>#D93939</span>
-      </div>
-      <div class="colour-block dark" style="background: #b31a1a;">
-        <strong>$ruby-700</strong>
-        <span>#B31A1A</span>
-      </div>
-    </div>
-
-    <br>
-
-    <div class="color-pallete">
-      <div class="colour-block" style="background: #e9f7e6;">
-        <strong>$emerald-50</strong>
-        <span>#E9F7E6</span>
-      </div>
-      <div class="colour-block dark" style="background: #24b33b;">
-        <strong>$emerald-300</strong>
-        <span>#24B33B</span>
-      </div>
-      <div class="colour-block dark" style="background: #0a884e;">
-        <strong>$emerald-500</strong>
-        <span>#0A884E</span>
-      </div>
-      <div class="colour-block dark" style="background: #086749;">
-        <strong>$emerald-700</strong>
-        <span>#086749</span>
-      </div>
-    </div>
-
-    <h3>Neutral</h3>
-    <p>Our neutral palette is based on an off-black with a subtle purple hue.</p>
-    <div class="colour-block desktop-only" style="border: 1px solid #f7f7f8; border-bottom: none;">
-      <strong>$white</strong>
-    </div>
-    <div class="color-pallete">
-      <div class="colour-block" style="border: 1px solid #f7f7f8;">
-        <strong><span class="mobile-only">$white / </span>$pearl-0</strong>
-        <span>#FFFFFF</span>
-      </div>
-      <div class="colour-block" style="background: #f7f7f8;">
-        <strong>$pearl-50</strong>
-        <span>#F7F7F8</span>
-      </div>
-      <div class="colour-block" style="background: #e4e4ea;">
-        <strong>$pearl-100</strong>
-        <span>#E4E4EA</span>
-      </div>
-      <div class="colour-block" style="background: #d0d0d9;">
-        <strong>$pearl-300</strong>
-        <span>#D0D0D9</span>
-      </div>
-      <div class="colour-block dark" style="background: #86868F;">
-        <strong>$pearl-500</strong>
-        <span>#86868F</span>
-      </div>
-      <div class="colour-block dark" style="background: #606068;">
-        <strong>$pearl-700</strong>
-        <span>#606068</span>
-      </div>
-      <div class="colour-block dark" style="background: #37373f;">
-        <strong><span class="mobile-only">$black / </span>$pearl-900</strong>
-        <span>#37373F</span>
-      </div>
-    </div>
-    <div class="colour-block dark desktop-only" style="background: #37373f;">
-      <strong >$black</strong>
-    </div>
-    <h2>Usage</h2>
+    <p>In an effort to bulletproof our system of colours, we use weights from 50 to 900, similar to font weights. Weight is defined by the contrast ratio of the colour against white, allowing us to convey relevant weights across our entire colour palette. This means <code>$pos-50</code> and <code>$neg-50</code> should be pretty close in weight, even if they’re on opposite ends of the colour spectrum.</p>
+    <li><router-link to="./">WealthBar</router-link></li>
+    <li><router-link to="assante">Assante Connect</router-link></li>
+    <router-view></router-view>
   </article>
 </template>
 <script>
-export default {};
+import wealthbar from './colour/index.vue'
+import assante from './colour/assante.vue'
+
+// const router = new VueRouter({
+//   routes: [
+//     { path: '/', component: User }
+//   ]
+// })
+
+export default {
+  // routes: [
+  //   { path: '/colour', component: wealthbar  },
+  //   { path: '/colour/assante', component: assante },
+  // ]
+};
+
 </script>
-<style lang="scss" scoped>
-
-  .colour-block {
-    padding: 0.5rem;
-
-    &.dark {
-      color: white;
-    }
-
-    span {
-      display: block;
-    }
-  }
-
-  @media #{$screen-width-medium} {
-    .color-pallete {
-      display: flex;
-      align-items: stretch;
-      justify-content: space-between;
-
-      .colour-block {
-        min-height: 6rem;
-        flex: 1 1 auto;
-      }
-
-      .colour-space {
-        width: 1rem;
-      }
-    }
-  }
-</style>
