45ca26efeb59e1f5a30e8d06648ae34f37e18636
Christoffer Johansen <christoffer.johansen@wealthbar.com>
2020-04-28 17:02:22 -0700

    Init peak showcase of new patterns

---
+++ src/views/banners.vue
@@ -0,0 +1,44 @@
+<template>
+  <article class="container">
+    <section>
+      <h1>Banners</h1>
+      <p>
+        Cards in Peak have been designed to act as a wrapper with some padding, and not much else. Everything outside of the
+        <code>&lt;div class="card-actions"&gt;</code> class, simply adheares to their usual style. Meaning it allows for 
+        consistent styling behaviour at all time when using elements or classes.
+      </p>
+    </section>
+
+    <section id="card">
+      <h3>Banners</h3>
+
+      <div class="banner light">
+        <div class="banner-content">
+          <h5>Test title</h5>
+          <button type="button" class="flat" icon>close</button>
+        </div>
+      </div>
+
+      <div class="code-sample">
+        <strong>Markup</strong>
+        <pre>
+          <code class="language-markup">&lt;div class="card"&gt;
+  &lt;h3&gt;Some h3 text&lt;/h3&gt;
+  &lt;p&gt;Some p text&lt;/p&gt;
+
+  &lt;div class="card-actions"&gt;
+    &lt;button class="outlined"&gt;Cancel&lt;/button&gt;
+    &lt;button type="submit"&gt;Next&lt;/button&gt;
+  &lt;/div&gt;
+&lt;/div&gt;</code>
+        </pre>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ src/views/cards.vue
@@ -0,0 +1,56 @@
+<template>
+  <article class="container">
+    <section>
+      <h1>Cards</h1>
+      <p>
+        Cards in Peak have been designed to act as a wrapper with some padding, and not much else. Everything outside of the
+        <code>&lt;div class="card-actions"&gt;</code> class, simply adheares to their usual style. Meaning it allows for 
+        consistent styling behaviour at all time when using elements or classes.
+      </p>
+    </section>
+
+    <section id="card">
+      <h3>Cards</h3>
+
+      <div class="card">
+        <h3>Example card</h3>
+
+        <div class="card-body">
+          <h5>Finance Ipsum</h5>
+          <p>
+            Securities default investment funds exchange market index value fall Fitch corporate 
+            bonds treasury hedge fund. Term risk management gains taxpayer upswing fall receive expenses 
+            volatile issuer rise. Term fiat 401k managed shares corporate bonds tax gains funds income maturities 
+            downturn yield retirement. Bills district Fitch municipal bonds exchange bull. 
+          </p>
+        </div>
+
+        <div class="card-actions">
+          <button type="submit" class="outline">Cancel</button>
+          <button type="submit">Next</button>
+        </div>
+      </div>
+
+      <div class="code-sample">
+        <strong>Markup</strong>
+        <pre>
+          <code class="language-markup">&lt;div class="card"&gt;
+  &lt;h3&gt;Some h3 text&lt;/h3&gt;
+  &lt;p&gt;Some p text&lt;/p&gt;
+
+  &lt;div class="card-actions"&gt;
+    &lt;button class="outlined"&gt;Cancel&lt;/button&gt;
+    &lt;button type="submit"&gt;Next&lt;/button&gt;
+  &lt;/div&gt;
+&lt;/div&gt;</code>
+        </pre>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
