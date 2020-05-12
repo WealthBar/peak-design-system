ed14e5bac59ba4f2013e6b276119a2c9d3d4fac0
Christoffer Johansen <christoffer.johansen@wealthbar.com>
2020-05-12 15:44:44 -0700

    Write comments for new components

--- src/views/workflows.vue
+++ src/views/workflows.vue
@@ -4,14 +4,83 @@
       <h1>Workflows</h1>
 
       <p>
-        ...
+        Workflows are forms broken into a multi-step process. The workflow visual is made
+        up of multiple Peak styles, and tied together with the workflow styles and animations.
       </p>
+
+      <ol>
+        <li>
+          <strong>Layouts:</strong>
+          .modal-page
+        </li>
+        <li>
+          <strong>Banners:</strong>
+          .modal-page-banner
+        </li>
+        <li><strong>Cards</strong></li>
+      </ol>
+
+      <p>
+        The workflow styles tie the above together styles together, and add animations styles to create the slide on navigation.
+      </p>
+
+       <ol>
+        <li>
+          .workflow
+        </li>
+        <li>
+          .workflow-form
+        </li>
+      </ol>
+
+      <p>
+        <strong>LEFT:</strong>
+        <br />
+        'slide-fade-in-right'
+        <br />
+        'slide-fade-out-right';
+      </p>
+
+      <p>
+        <strong>RIGHT:</strong>
+        <br />
+        'slide-fade-in-left'
+        <br />
+        'slide-fade-out-left'
+      </p>
+
+      <div class="code-sample">
+        <pre>
+          <code class="language-markup">### TEMPLATE ###
+
+&lt;form class="workflow-form"&gt;
+  &lt;transition :enter-active-class="enterClasses" :leave-active-class="leaveClasses"&gt;
+    &lt;div class="step"&gt;
+      &lt;div class="card"&gt;
+        ...
+      &lt;/div&gt;
+    &lt;/div&gt;
+  &lt;/transition&gt;
+&lt;form&gt;
+
+### VUE COMPONENT ###
+
+prev(step) {
+  this.enterClasses = 'slide-fade-in-right';
+  this.leaveClasses = 'slide-fade-out-right';
+},
+
+next(step) {
+  this.enterClasses = 'slide-fade-in-left';
+  this.leaveClasses = 'slide-fade-out-left';
+},</code>
+        </pre>
+      </div>
     </section>
 
-    <h3>Workflows</h3>
 
     <div class="modal-page workflow">
-      <div class="modal-page-banner light">
+      <div class="modal-page-banner">
         <div class="banner-content">
           <h5 class="title">Test title</h5>
           <button type="button" class="flat action" icon>Close</button>
@@ -19,7 +88,7 @@
       </div>
 
       <form class="workflow-form" v-on:submit.prevent>
-        <transition :enter-active-class="enterClasses" :leave-active-class="leaveClasses" :duration="500">
+        <transition :enter-active-class="enterClasses" :leave-active-class="leaveClasses">
           <div class="step" v-if="step === 1" :key="step">
             <div class="card">
               <h3>Step 1</h3>
@@ -77,7 +146,6 @@
     &lt;transition 
       :enter-active-class="enterClasses" 
       :leave-active-class="leaveClasses" 
-      :duration="500"
     &gt;
       &lt;div class="step" v-if="step === 1" :key="step"&gt;
         &lt;div class="card"&gt;
