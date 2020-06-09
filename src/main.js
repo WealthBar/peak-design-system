216298d836a83cdce55631e22cea8d6e18c78ba3
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2020-06-09 13:51:56 -0700

    swap vue-prism for prismjs

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Meta from 'vue-meta';
 
-import VuePrism from 'vue-prism';
+import prism from 'prismjs';
 import app from './app';
 import router from './router';
 import store from './store';
@@ -11,9 +11,9 @@ import store from './store';
 /* istanbul ignore next: bootstrap code */
 (function bootstrap() {
   Vue.use(Router);
-  Vue.use(VuePrism);
   Vue.use(Meta, { keyName: 'head' });
   Vue.config.productionTip = false;
+  Vue.mixin({ mounted() { prism.highlightAll(); } });
 
   /* eslint-disable no-new */
   new Vue({
