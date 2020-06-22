25eeb483a6f93e5d9e8f0ebbf3ccddd26f7114d1
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2020-06-22 15:34:54 -0700

    update headers page

--- src/views/typography/header.vue
+++ src/views/typography/header.vue
@@ -2,119 +2,76 @@
   <article class="container">
     <header>
       <h1>Header Text</h1>
-
       <div class="toggles">
-        <label for="screen-toggle">Screen Variant</label>
-        <button id="screen-toggle" class="toggle" @click="toggleScreen">
-          <span
-            v-for="screen in getScreens"
-            :key="screen"
-            :data-active="getSelectedScreen === screen"
-          >{{ screen }}</span>
-        </button>
+        <div>
+          <label for="screen-toggle">Screen Variant</label>
+          <button id="screen-toggle" class="toggle" @click="toggleScreen">
+            <span
+              v-for="screen in getScreens"
+              :key="screen"
+              :data-active="getSelectedScreen === screen"
+            >{{ screen }}</span>
+          </button>
+        </div>
+        <div>
+          <label for="margin-toggle">Show Margins</label>
+          <button id="margin-toggle" class="toggle" @click="showMargin = !showMargin">
+            <span :data-active="!showMargin">off</span>
+            <span :data-active="showMargin">on</span>
+          </button>
+        </div>
       </div>
     </header>
     <section>
-      <h2 id="headers"></h2>
-      <p>Headers create hierarchy in a layout and make scanning easy. Use them for content such as page titles or section titles. We have <strong>mobile</strong> and <strong>desktop</strong> header styles controlled by media queries based on browser's screen size. This ensures that our header typography is responsive to screen real estate.</p>
-
+      <p>Headers create hierarchy in a layout and make scanning easy. Use them for content such as page titles or section titles. We have <strong>mobile</strong> and <strong>desktop</strong> header styles controlled by media queries based on browser's screen size. This ensures that our header typography is responsive to screen real estate. All header elements have a bottom margin of <code>1rem</code> and maintains standard browser <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Box_Model/Mastering_margin_collapsing">margin collapse principles</a>.</p>
+    </section>
+    <section>
       <!-- <h3>H1 header</h3> -->
-      <div class="example">
+      <div :class="showMargin ? 'example' : ''">
         <h1 :class="getSelectedScreen">This is a super large H1 header.</h1>
-      </div>
-      <div class="attributes">
-        <div class="code-sample">
-          <strong>Markup</strong>
-          <pre><code class="language-markup">&lt;h1&gt;This is a super large H1 header.&lt;/h1&gt;</code></pre>
-        </div>
-        <div class="code-sample">
-          <strong>Style</strong>
-          <pre class="language-css" :class="getSelectedUnit"><code>{{ headerStyle('h1') }}</code></pre>
-        </div>
-      </div>
-
-      <!-- <h3>H2 header</h3> -->
-      <div class="example">
         <h2 :class="getSelectedScreen">This is a huge H2 header.</h2>
-      </div>
-      <div class="attributes">
-        <div class="code-sample">
-          <strong>Markup</strong>
-          <pre><code class="language-markup">&lt;h2&gt;This is a huge H2 header.&lt;/h2&gt;</code></pre>
-        </div>
-        <div class="code-sample">
-          <strong>Style</strong>
-          <pre class="language-css" :class="getSelectedUnit"><code>{{ headerStyle('h2') }}</code></pre>
-        </div>
-      </div>
-
-      <!-- <h3>H3 header</h3> -->
-      <div class="example">
         <h3 :class="getSelectedScreen">This is a big H3 header.</h3>
-      </div>
-      <div class="attributes">
-        <div class="code-sample">
-          <strong>Markup</strong>
-          <pre><code class="language-markup">&lt;h3&gt;This is a big H3 header.&lt;/h3&gt;</code></pre>
-        </div>
-        <div class="code-sample">
-          <strong>Style</strong>
-          <pre class="language-css" :class="getSelectedUnit"><code>{{ headerStyle('h3') }}</code></pre>
-        </div>
-      </div>
-
-      <!-- <h3>H4 header</h3> -->
-      <div class="example">
         <h4 :class="getSelectedScreen">This is a medium H4 header.</h4>
-      </div>
-      <div class="attributes">
-        <div class="code-sample">
-          <strong>Markup</strong>
-          <pre><code class="language-markup">&lt;h4&gt;This is a medium H4 header.&lt;/h4&gt;</code></pre>
-        </div>
-        <div class="code-sample">
-          <strong>Style</strong>
-          <pre class="language-css" :class="getSelectedUnit"><code>{{ headerStyle('h4') }}</code></pre>
-        </div>
-      </div>
-
-      <!-- <h3>H5 header</h3> -->
-      <div class="example">
         <h5 :class="getSelectedScreen">This is a small H5 header.</h5>
-      </div>
-      <div class="attributes">
-        <div class="code-sample">
-          <strong>Markup</strong>
-          <pre><code class="language-markup">&lt;h5&gt;This is a small H5 header.&lt;/h5&gt;</code></pre>
-        </div>
-        <div class="code-sample">
-          <strong>Style</strong>
-          <pre class="language-css" :class="getSelectedUnit"><code>{{ headerStyle('h5') }}</code></pre>
-        </div>
-      </div>
-
-      <!-- <h3>H6 header</h3> -->
-      <div class="example">
         <h6 :class="getSelectedScreen">This is a very small H6 header.</h6>
       </div>
       <div class="attributes">
         <div class="code-sample">
           <strong>Markup</strong>
-          <pre><code class="language-markup">&lt;h6&gt;This is a very small H6 header.&lt;/h6&gt;</code></pre>
-        </div>
-        <div class="code-sample">
-          <strong>Style</strong>
-          <pre class="language-css" :class="getSelectedUnit"><code>{{ headerStyle('h6') }}</code></pre>
+          <pre>
+            <code class="language-markup">&lt;h1&gt;This is a super large H1 header.&lt;/h1&gt;
+&lt;h2&gt;This is a huge H2 header.&lt;/h2&gt;
+&lt;h3&gt;This is a big H3 header.&lt;/h3&gt;
+&lt;h4&gt;This is a medium H4 header.&lt;/h4&gt;
+&lt;h5&gt;This is a small H5 header.&lt;/h5&gt;
+&lt;h6&gt;This is a very small H6 header.&lt;/h6&gt;</code>
+            </pre>
         </div>
       </div>
-
-      <hr />
-
+    </section>
+    <section>
+      <div :class="showMargin ? 'example' : ''">
+        <h2 :class="getSelectedScreen">Header Spacing Example</h2>
+        <p>This is my paragraph of text. this is standard text and is being used to illustrate how margins work with realtion to headers and other elements</p>
+        <h5 :class="getSelectedScreen">A Smaller Sub-head</h5>
+        <p>This is my paragraph of text. this is standard text and is being used to illustrate how margins work with realtion to headers and other elements</p>
+      </div>
+      <div class="attributes">
+        <div class="code-sample">
+          <strong>Markup</strong>
+          <pre><code class="language-markup">&lt;h2&gt;Huge Header Spacing Example&lt;/h2&gt;
+&lt;p&gt;This is my paragraph of text. … &lt;/p&gt;
+&lt;h3&gt;Smaller Sub-head Example&lt;/h3&gt;
+&lt;p&gt;This is my paragraph of text. … &lt;/p&gt;</code></pre>
+        </div>
+      </div>
+    </section>
+    <section>
       <h3>Remove Margins</h3>
       <p>
         <strong>Use only as required by design.</strong> Default margins on any header or block element (eg: <code>h2</code> or <code>p</code>) can be removed by adding a class of <code>no-margin</code> to it.
       </p>
-      <div class="example">
+      <div :class="showMargin ? 'example' : ''">
         <h3 class="no-margin">This is H3 element has no margin.</h3>
       </div>
       <strong>Markup</strong>
@@ -132,7 +89,7 @@ export default {
     return {
       name: 'typography',
       title: 'Typography',
-      isMobile: false,
+      showMargin: false,
       typography,
     };
   },
@@ -324,15 +281,27 @@ header {
   justify-content: space-between;
 }
 
+.toggles{
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  div {
+    margin-left: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+  }
+}
+
 section {
-  padding-bottom: 2rem;
   border-bottom: 1px solid $neutral-100;
 }
 
 .attributes {
   display: flex;
   flex-direction: column;
-  margin-bottom: 1rem;
+  // margin-bottom: 1rem;
 
   @media #{$screen-width-large} {
     flex-direction: row;
@@ -340,57 +309,53 @@ section {
 }
 
 .code-sample {
-  display: flex;
-  flex-direction: column;
-  @media #{$screen-width-large} {
-    flex: 1 1 50%;
-    &:last-child {
-      padding-left: 1rem;
-    }
+  width: 100%;
+  // display: flex;
+  // flex-direction: column;
+  // @media #{$screen-width-large} {
+  //   flex: 1 1 50%;
+  //   &:last-child {
+  //     padding-left: 1rem;
+  //   }
     pre {
-      flex: 1 1 50%;
-      display: flex;
+      // flex: 1 1 50%;
+      // display: block;
+      // margin: 0;
     }
-  }
+  // }
 }
 
 .example {
   background: $tertiary-50;
-  border-top: 1px solid $tertiary-100;
-  border-bottom: 1px solid $tertiary-100;
-  margin-bottom: 1rem;
+  display: inline-block;
+  width: 100%;
+  box-shadow: inset 0 1px 0 0 $tertiary-100, inset 0 -1px 0 0 $tertiary-100;
   > h1,
   > h2,
   > h3,
   > h4,
   > h5,
-  > h6 {
-    background: $white;
-    border-top: 1px solid $tertiary-100;
-    border-bottom: 1px solid $tertiary-100;
-  }
+  > h6,
   > p,
   > strong,
   > small,
   > .small,
   > .subtle {
     background: $white;
-    border-top: 1px solid $tertiary-100;
-    border-bottom: 1px solid $tertiary-100;
-    &:first-child {
-      border-top: none;
-    }
+    box-shadow: 0 1px 0 0 $tertiary-100, 0 -1px 0 0 $tertiary-100;
+    // border-top: 1px solid $tertiary-100;
+    // border-bottom: 1px solid $tertiary-100;
   }
   ul,
   ol {
     background: $tertiary-50;
     border-top: none;
-    border-bottom: 1px solid $tertiary-100;
+    // border-bottom: 1px solid $tertiary-100;
     li {
       background: $white;
       ul,
       ol {
-        border-top: 1px solid $tertiary-100;
+        // border-top: 1px solid $tertiary-100;
         border-bottom: none;
       }
     }
