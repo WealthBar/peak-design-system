8dc4312df948342b4cd7c404b58966b291af4aca
Christoffer Johansen <christoffer.johansen@wealthbar.com>
2020-07-02 12:46:15 -0700

    Update card description, and flip notice code example and visual

--- src/views/layout_styles.vue
+++ src/views/layout_styles.vue
@@ -9,12 +9,8 @@
         The <code>.card</code> is visual treatment class which can be applied to any block element to get the desired card look.
         There is no inner margin/padding — this is provided by internal containers and content. The card class is
         also used with other style patterns to provide a complete structure for content. Some examples of these
-        patterns are the pattern and several (upcoming) table styles:
+        patterns are the <a href="http://localhost:8020/workflows#step">Workflow step</a> pattern and several (upcoming) table styles:
       </p>
-
-      <ol>
-        <li><a href="http://localhost:8020/workflows#step">Workflow step</a></li>
-      </ol>
 
       <div class="card">
         <p>
@@ -38,20 +34,21 @@
       <h2>Notice</h2>
       <p>Notice patterns are designed as an adaptable pattern for use in a variety of contexts from callouts to toast notifications. Adding a <code>notice</code> class to the containing element provides a default <code>$bg</code> coloured notice. Variants include <code>alt</code>, <code>light</code>, <code>alert</code>, and <code>neg</code>. For ease of building the <code>notice-content</code> class — as well as <code>&lt;header&gt;</code>, <code>&lt;article&gt;</code>, or <code>&lt;footer&gt;</code> elements inside the notice container will have a have side margins applied.</p>
       <h3>Default</h3>
-      <pre><code class="language-markup">&lt;div class="notice"&gt;
-  &lt;p class="notice-content"&gt; … &lt;/p&gt;
-&lt;/div&gt;</code></pre>
 
       <div class="notice">
         <p class="notice-content">
           {{ noticeCopy }}
         </p>
       </div>
-      <hr>
-      <h3>Alternate</h3>
-      <pre><code class="language-markup">&lt;div class="notice alt"&gt;
-  &lt;article&gt; … &lt;/article&gt;
+
+      <pre><code class="language-markup">&lt;div class="notice"&gt;
+  &lt;p class="notice-content"&gt; … &lt;/p&gt;
 &lt;/div&gt;</code></pre>
+
+      <hr>
+
+      <h3>Alternate</h3>
+
       <div class="notice alt">
         <article>
           <p>
@@ -59,12 +56,15 @@
           </p>
         </article>
       </div>
-      <hr>
-      <h3>Light</h3>
-      <pre><code class="language-markup">&lt;div class="notice light"&gt;
-  &lt;header&gt; … &lt;/header&gt;
-  &lt;footer&gt; … &lt;/footer&gt;
+
+      <pre><code class="language-markup">&lt;div class="notice alt"&gt;
+  &lt;article&gt; … &lt;/article&gt;
 &lt;/div&gt;</code></pre>
+
+      <hr>
+
+      <h3>Light</h3>
+
       <div class="notice light">
         <header>
           <h3>H3 Header</h3>
@@ -75,11 +75,16 @@
           </p>
         </footer>
       </div>
-      <hr>
-      <h3>Alert</h3>
-      <pre><code class="language-markup">&lt;div class="notice alert"&gt;
-  &lt;article&gt; … &lt;/article&gt;
+
+      <pre><code class="language-markup">&lt;div class="notice light"&gt;
+  &lt;header&gt; … &lt;/header&gt;
+  &lt;footer&gt; … &lt;/footer&gt;
 &lt;/div&gt;</code></pre>
+
+      <hr>
+
+      <h3>Alert</h3>
+
       <div class="notice alert">
         <article>
           <p>
@@ -95,11 +100,16 @@
           </ul>
         </article>
       </div>
-      <hr>
-      <h3>Negative</h3>
-      <pre><code class="language-markup">&lt;div class="notice neg"&gt;
+
+      <pre><code class="language-markup">&lt;div class="notice alert"&gt;
   &lt;article&gt; … &lt;/article&gt;
 &lt;/div&gt;</code></pre>
+
+
+      <hr>
+
+      <h3>Negative</h3>
+
       <div class="notice neg">
         <article>
           <p>
@@ -107,6 +117,10 @@
           </p>
         </article>
       </div>
+
+      <pre><code class="language-markup">&lt;div class="notice neg"&gt;
+  &lt;article&gt; … &lt;/article&gt;
+&lt;/div&gt;</code></pre>
     </section>
     <!-- NOTICE END -->
   </article>
