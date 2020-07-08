a93d942f5b41b6bf944e4013426eb9c52a640dd1
Nathan Kondra <nathan.kondra@wealthbar.com>
2020-07-08 15:46:29 -0700

    Prevent linting changes

--- src/views/layout_styles.vue
+++ src/views/layout_styles.vue
@@ -7,42 +7,23 @@
       <h2>Card</h2>
 
       <h3>Usage Guidelines</h3>
-      <p>
-        The <code>.card</code> class can be applied to any block element to
-        provide visual treatment. <code>.card</code> has no inner margin/padding
-        — this is provided by internal containers and content.
-      </p>
-      <p>
-        The card class can be combined with other style patterns to provide a
-        complete structure for content. Some examples of these patterns are the
-        <a href="/workflows#step">Workflow Step</a> pattern and several
-        (upcoming) table styles.
-      </p>
-      <p>
-        There are two variants of <code>.card</code>: a default and a variant
-        with an added box-shadow. Generally, we use the default against a white
-        background and the shadow variant against a light grey background.
-      </p>
+      <p>The <code>.card</code> class can be applied to any block element to provide visual treatment. <code>.card</code> has no inner margin/padding — this is provided by internal containers and content.</p>
+      <p>The card class can be combined with other style patterns to provide a complete structure for content. Some examples of these patterns are the <a href="/workflows#step">Workflow Step</a> pattern and several (upcoming) table styles.</p>
+      <p>There are two variants of <code>.card</code>: a default and a variant with an added box-shadow. Generally, we use the default against a white background and the shadow variant against a light grey background.</p>
 
       <h3>Default</h3>
       <div class="card example">
-        <p>
-          Card in its simplest form. Margin is applied to the content. This
-          basic card is often used against a white background.
-        </p>
+        <p>Card in its simplest form. Margin is applied to the content. This basic card is often used against a white background.</p>
       </div>
       <pre><code class="language-markup">&lt;div class="card"&gt;
   &lt;p&gt;Card in its simplest form.&lt;/p&gt;
 &lt;/div&gt;</code></pre>
 
-      <hr />
+      <hr>
 
       <h3>Box Shadow</h3>
       <div class="card shadow example">
-        <p>
-          Card with box shadow. Margin is applied to the content. This
-          box-shdaow variant is generally used on a grey background.
-        </p>
+        <p>Card with box shadow. Margin is applied to the content. This box-shdaow variant is generally used on a grey background.</p>
       </div>
       <pre><code class="language-markup">&lt;div class="card shadow"&gt;
   &lt;p&gt;Card with box shadow.&lt;/p&gt;
@@ -50,22 +31,12 @@
     </section>
     <!-- CARD END -->
 
-    <hr />
+    <hr>
 
     <!-- NOTICE -->
     <section id="notice">
       <h2>Notice</h2>
-      <p>
-        Notice patterns are designed as an adaptable pattern for use in a
-        variety of contexts from callouts to toast notifications. Adding a
-        <code>notice</code> class to the containing element provides a default
-        <code>$bg</code> coloured notice. Variants include <code>alt</code>,
-        <code>light</code>, <code>alert</code>, and <code>neg</code>. For ease
-        of building the <code>notice-content</code> class — as well as
-        <code>&lt;header&gt;</code>, <code>&lt;article&gt;</code>, or
-        <code>&lt;footer&gt;</code> elements inside the notice container will
-        have a have side margins applied.
-      </p>
+      <p>Notice patterns are designed as an adaptable pattern for use in a variety of contexts from callouts to toast notifications. Adding a <code>notice</code> class to the containing element provides a default <code>$bg</code> coloured notice. Variants include <code>alt</code>, <code>light</code>, <code>alert</code>, and <code>neg</code>. For ease of building the <code>notice-content</code> class — as well as <code>&lt;header&gt;</code>, <code>&lt;article&gt;</code>, or <code>&lt;footer&gt;</code> elements inside the notice container will have a have side margins applied.</p>
       <h3>Default</h3>
 
       <div class="notice">
@@ -78,7 +49,7 @@
   &lt;p class="notice-content"&gt; … &lt;/p&gt;
 &lt;/div&gt;</code></pre>
 
-      <hr />
+      <hr>
 
       <h3>Alternate</h3>
 
@@ -94,7 +65,7 @@
   &lt;article&gt; … &lt;/article&gt;
 &lt;/div&gt;</code></pre>
 
-      <hr />
+      <hr>
 
       <h3>Light</h3>
 
@@ -114,7 +85,7 @@
   &lt;footer&gt; … &lt;/footer&gt;
 &lt;/div&gt;</code></pre>
 
-      <hr />
+      <hr>
 
       <h3>Alert</h3>
 
@@ -138,7 +109,8 @@
   &lt;article&gt; … &lt;/article&gt;
 &lt;/div&gt;</code></pre>
 
-      <hr />
+
+      <hr>
 
       <h3>Negative</h3>
 
@@ -172,17 +144,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-<<<<<<< HEAD
-#card {
-  .example {
-    margin-bottom: 1.25rem;
-
-    p {
-      margin: 1rem 1.5rem;
-    }
-  }
-}
-=======
   #card {
     .example {
       margin-bottom: 1.25rem;
@@ -192,5 +153,4 @@ export default {
       }
     }
   }
->>>>>>> Revert bumped layout_styles.vue
 </style>
