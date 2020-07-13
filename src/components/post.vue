8ff9d68eeac33b4e171f79c7741bd77cf6bb428a
Geoff Phillips <geoffrey.phillips@wealthbar.com>
2020-07-13 14:29:33 -0700

    Move documentation to separate tab

---
+++ src/components/post.vue
@@ -0,0 +1,125 @@
+<template>
+  <article class="post">
+    <section class="post-status">
+      <div class="status-icon" :class="iconVariant">
+        <svg v-if="action === 'create'" viewBox="0 0 32 32">
+          <polygon points="28,15 17,15 17,4 15,4 15,15 4,15 4,17 15,17 15,28 17,28 17,17 28,17"/>
+        </svg>
+        <svg v-else-if="action.includes('assign')" viewBox="0 0 32 32">
+          <path d="M16,16c-3.9,0-7-3.1-7-7s3.1-7,7-7s7,3.1,7,7S19.9,16,16,16z M16,4c-2.8,0-5,2.2-5,5s2.2,5,5,5s5-2.2,5-5S18.8,4,16,4z"/>
+          <path d="M29,30H3v-5.1c0-2.2,1.2-4.1,3.2-5.1c6.1-3,13.6-3,19.7,0c2,1,3.2,2.9,3.2,5.1V30z M5,28h22v-3.1c0-1.4-0.8-2.7-2.1-3.3 c-5.5-2.8-12.4-2.8-17.9,0C5.8,22.2,5,23.5,5,24.9V28z"/>
+        </svg>
+        <svg v-else-if="action === 'approve'" viewBox="0 0 32 32">
+          <polygon points="11,27.4 1.3,17.7 2.7,16.3 11,24.6 29.3,6.3 30.7,7.7"/>
+        </svg>
+        <svg v-else-if="['reject', 'cancel'].includes(action)" viewBox="0 0 32 32">
+          <polygon points="26.7,6.7 25.3,5.3 16,14.6 6.7,5.3 5.3,6.7 14.6,16 5.3,25.3 6.7,26.7 16,17.4 25.3,26.7 26.7,25.3 17.4,16 "/>
+        </svg>
+        <p v-else class="no-margin">{{ actor[0] }}</p>
+      </div>
+      <div class="status-details">
+        <span><strong>{{ actor }}</strong> {{ status }}</span>
+        <time class="subtle">{{ timeString }}</time>
+      </div>
+    </section>
+    <p v-if="comment" class="post-comment small no-margin">{{ comment }}</p>
+  </article>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  props: {
+    comment: {
+      type: String,
+      default: '',
+    },
+    action: {
+      type: String,
+      default: '',
+    },
+    actor: {
+      type: String,
+      default: '',
+    },
+    timeString: {
+      type: String,
+      default: '',
+    },
+    status: {
+      type: String,
+      default: '',
+    }
+  },
+  computed: {
+    iconVariant() {
+      return ['reject', 'cancel', 'approve'].includes(this.action) ?
+        'dark'
+      : this.action === 'comment' ?
+        ''
+      :
+        'subtle'; 
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    margin-bottom: 1rem;
+    
+    .post-status {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      .status-icon {
+        height: 2rem;
+        width: 2rem;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: $bg-50;
+        color: $primary-500;
+
+        &.dark {
+          background-color: $black;
+      
+          svg {
+            fill: $white;
+          }
+        }
+      
+        &.subtle {
+          background-color: $secondary-50;
+          
+          svg {
+            fill: $secondary-500;
+          }
+        }
+
+        svg {
+          height: 1rem;
+          width: 1rem;
+        }
+      }
+
+      .status-details {
+        font-size: 0.875rem;
+        display: flex;
+        flex-direction: column;
+        margin-left: 0.75rem;
+      } 
+    }
+
+    .post-comment {
+      margin: 1rem 0 0 2.75rem;
+      border-left: 1px solid $neutral-100;
+      padding-left: 1rem;
+      white-space: pre-line;
+      word-break: break-word;
+    }
+  }
+</style>
---
+++ src/views/posts.vue
@@ -0,0 +1,115 @@
+<template>
+  <article class="container">
+    <section id="post">
+      <h1>Post</h1>
+
+      <p>A post is a layout pattern used to display staff activities on various admin interfaces.</p>
+
+      <p>The Peak implementation of <code>post</code> should enable the end developer to easily mark up a timeline showing multiple points of activity or multiple comments using <code>v-for</code> or something akin to it in future frameworks. As such, post should display itself in one of two variants, depending on what data it is given:</p>
+
+      <ul>
+        <li>Status variant - Show a basic version featuring a status update.</li>
+        <li>Comment variant - Render an additional block of text, typically the body of a comment.</li>
+      </ul>
+
+      <h3>Status Variant</h3>
+
+      <post 
+        v-for="(post, index) in statuses"
+        :key="`${index}status`"
+        :action="post.action"
+        :actor="post.actor"
+        :status="post.status"
+        :time-string="post.timeString"
+      ></post>
+
+      <pre><code class="language-markup">&lt;article class="post"&gt;
+  &lt;section class="status"&gt;
+    &lt;div class="icon dark"&gt;
+      &lt;svg viewBox="0 0 32 32"&gt;
+        &lt;polygon points="11,27.4 1.3,17.7 2.7,16.3 11,24.6 29.3,6.3 30.7,7.7" /&gt;
+      &lt;/svg&gt;
+    &lt;/div&gt;
+    &lt;div class="status-details"&gt;
+      &lt;span&gt;&lt;strong&gt;User&lt;/strong&gt; approved the request.&lt;/span&gt;
+      &lt;time class="subtle"&gt;Jan 1 - 1:11pm&lt;/time&gt;
+    &lt;/div&gt;
+  &lt;/section&gt;
+&lt;/article&gt;</code></pre>
+
+      <h3>Comment Variant</h3>
+
+      <post 
+        v-for="(post, index) in comments"
+        :key="`${index}comment`"
+        :action="post.action"
+        :actor="post.actor"
+        :status="post.status"
+        :time-string="post.timeString"
+        :comment="post.comment"
+      ></post>
+
+      <pre><code class="language-markup">&lt;article class="post"&gt;
+  &lt;section class="main"&gt;
+    &lt;div class="icon"&gt;
+      &lt;p class="no-margin"&gt;U&lt;/p&gt;
+    &lt;/div&gt;
+    &lt;div class="status-details"&gt;
+      &lt;span&gt;&lt;strong&gt;User&lt;/strong&gt; left a comment.&lt;/span&gt;
+      &lt;time class="subtle"&gt;Jan 2 - 2:00pm&lt;/time&gt;
+    &lt;/div&gt;
+  &lt;/section&gt;
+  &lt;p class="comment small no-margin"&gt;This is a comment\nIt can be multi-line with an unescaped "\n"&lt;/p&gt;
+&lt;/article&gt;</code></pre>
+
+      <p>Our <code>post</code> component accepts the props below. Please note that the component does not localize text. The final text to be displayed - localized if necessary - must be passed to the component</p>
+
+      <ul>
+        <li><code>action</code>: A string that describes type of status update to display. The component has some internal logic to change the svg and its color based on the action. Current accepted actions are <code>'approve'</code>, <code>'assign'</code>, <code>'cancel'</code>, <code>'comment'</code>, <code>'create'</code>, <code>'reject'</code>, <code>'unassign'</code>.</li>
+        <li><code>actor</code>: A string that is name of the user who took the action.</li>
+        <li><code>time-string</code>: The string representing the time the action was taken.</li>
+        <li><code>status</code>: The specific text of status update to be displayed.</li>
+        <li><code>comment</code> (optional): If passed in as a prop, will render an additional block of text below the status update.</li>
+      </ul>
+
+      <pre><code class="language-markup">&lt;post
+  v-for="(post, index) in posts"
+  :key="`${index}post`"
+  :action="post.action"
+  :actor="post.actor"
+  :status="post.status"
+  :time-string="post.timeString"
+  :comment="post.comment"
+&gt;&lt;/post&gt;</code></pre>
+    </section>
+  </article>
+</template>
+
+<script>
+import post from '@/components/post';
+
+export default {
+  components: { post },
+  data() {
+    return {
+      statuses: [
+        {
+          actor: 'Staff #1',
+          action: 'approve',
+          status: 'approved the request.',
+          timeString: 'Jan 1 - 1:11PM',
+        },
+      ],
+      comments: [
+        {
+          actor: 'Staff #2',
+          action: 'comment',
+          status: 'left a comment.',
+          timeString: 'Jan 2 - 2:00PM',
+          comment: 'This is a comment\nIt can be multi-line with an unescaped "\\n"'
+        },
+      ],
+    };
+  },
+};
+</script>
