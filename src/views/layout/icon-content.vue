990d1e02eb46a298adff5b98d0618d08aec556e7
Travis Rempel <travis.rempel@wealthbar.com>
2020-08-14 13:09:20 -0700

    Simplify markup structure

--- src/views/layout/icon-content.vue
+++ src/views/layout/icon-content.vue
@@ -10,44 +10,36 @@
 
     <div class="card shadow example">
       <div class="icon-content">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>Default</strong>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">Default</p>
         <p>The Icon Content pattern allows for an SVG Icon to be used like a bullet on a card's title. Paragraphs and any other content under that title become indented to match the indent of the title text.</p>
-        <p>In responsive mobile, left padding collapses.</p>
+        <p>In responsive mobile, body indenting is removed.</p>
       </div>
       
       <hr>
 
       <div class="icon-content">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <span>The Icon Content pattern can also be applied directly to body copy without a title.</span>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p>The Icon Content pattern can also be applied directly to body copy without a title.</p>
       </div>
     </div>
 
     <pre><code class="language-markup">&lt;div class="card shadow"&gt;
   &lt;div class="icon-content"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;Default&lt;/strong&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;Default&lt;/p&gt;
     &lt;p&gt;Paragraphs and any other content under that title...&lt;/p&gt;
   &lt;/div&gt;
 
   &lt;hr&gt;
 
   &lt;div class="icon-content"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;span&gt;The Icon Content pattern can also be applied directly to body copy without a title.&lt;/span&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p&gt;The Icon Content pattern can also be applied directly to body copy without a title.&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;</code></pre>
 
@@ -55,66 +47,54 @@
 
     <div class="card shadow example">
       <div class="icon-content small">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>Small</strong>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">Small</p>
         <p>Use the class <code class="small">.small</code> to get a smaller 0.875rem (14px) font size and tighter leading.</p>
       </div>
 
       <hr>
 
       <div class="icon-content large-margin">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>Large Margin</strong>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">Large Margin</p>
         <p>Use the class <code>.large-margin</code> to get a larger 1.5rem (24px) margin around content at desktop sizes.</p>
       </div>
 
       <hr>
 
       <div class="icon-content no-indent">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>No Indent</strong>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">No Indent</p>
         <p>Use the class <code>.no-indent</code> to remove indenting from content. Note this this happens by default on mobile sizes.</p>
       </div>
     </div>
 
     <pre><code class="language-markup">&lt;div class="card shadow"&gt;
-  &lt;div class="icon-content small"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;Small&lt;/strong&gt;
-    &lt;/p&gt;
+  &lt;div class="icon-content small"&gt;  
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;Small&lt;/p&gt;
     &lt;p&gt;A smaller 0.875rem (14px) font size and tighter leading.&lt;/p&gt;
   &lt;/div&gt;
 
   &lt;hr&gt;
 
   &lt;div class="icon-content large-margin"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;Large Margin&lt;/strong&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;Large Margin&lt;/p&gt;
     &lt;p&gt;A larger 1.5rem (24px) margin around content at desktop sizes.&lt;/p&gt;
   &lt;/div&gt;
 
   &lt;hr&gt;
 
   &lt;div class="icon-content no-indent"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;No Indent&lt;/strong&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;No Indent&lt;/p&gt;
     &lt;p&gt;Remove indenting from content.&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;</code></pre>
@@ -125,45 +105,37 @@
     <p>Our Notices are designed to display a small amount of information. They work well with a single instance of <code>icon-content</code>.</p>
 
     <div class="notice">
-      <div class="icon-content">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>Default</strong>
-        </p>
+      <div class="icon-content">   
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">Default</p>
         <p>The Icon Content pattern allows for an SVG Icon to be used like a bullet on a notice's title. Paragraphs and any other content under that title become indented to match the indent of the title text.</p>
-        <p>In responsive mobile, left padding collapses.</p>
+        <p>In responsive mobile, body indenting is removed.</p>
       </div>
     </div>
 
     <div class="notice">
       <div class="icon-content">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <span>The Icon Content pattern can also be applied directly to body copy without a title.</span>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p>The Icon Content pattern can also be applied directly to body copy without a title.</p>
       </div>
     </div>
 
         <pre><code class="language-markup">&lt;div class="notice"&gt;
   &lt;div class="icon-content"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;Default&lt;/strong&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;Default&lt;/p&gt;
     &lt;p&gt;Paragraphs and any other content under that title...&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;
 
 &lt;div class="notice"&gt;
   &lt;div class="icon-content"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;span&gt;The Icon Content pattern can also be applied directly to body copy without a title.&lt;/span&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="span"&gt;The Icon Content pattern can also be applied directly to body copy without a title.&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;</code></pre>
 
@@ -171,66 +143,54 @@
 
     <div class="notice">
       <div class="icon-content small">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>Small</strong>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">Small</p>
         <p>Use the class <code class="small">.small</code> to get a smaller 0.875rem (14px) font size and tighter leading.</p>
       </div>
     </div>
 
     <div class="notice">
       <div class="icon-content large-margin">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>Large Margin</strong>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">Large Margin</strong>
         <p>Use the class <code>.large-margin</code> to get a larger 1.5rem (24px) margin around content at desktop sizes.</p>
       </div>
     </div>
 
     <div class="notice">
       <div class="icon-content no-indent">
-        <p class="icon-title">
-          <svg viewBox="0 0 16 16" title="Information">
-            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-          </svg>
-          <strong>No Indent</strong>
-        </p>
+        <svg viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <p class="strong">No Indent</p>
         <p>Use the class <code>.no-indent</code> to remove indenting from content. Note this this happens by default on mobile sizes.</p>
       </div>
     </div>
 
     <pre><code class="language-markup">&lt;div class="notice"&gt;
   &lt;div class="-content small"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;Small&lt;/strong&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;Small&lt;/p&gt;
     &lt;p&gt;A smaller 0.875rem (14px) font size and tighter leading.&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;
 
 &lt;div class="notice"&gt;
   &lt;div class="icon-content large-margin"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;Large Margin&lt;/strong&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;Large Margin&lt;/p&gt;
     &lt;p&gt;A larger 1.5rem (24px) margin around content at desktop sizes.&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;
 
 &lt;div class="notice"&gt;
   &lt;div class="icon-content no-indent"&gt;
-    &lt;p class="icon-title"&gt;
-      &lt;svg&gt;...&lt;/svg&gt;
-      &lt;strong&gt;No Indent&lt;/strong&gt;
-    &lt;/p&gt;
+    &lt;svg&gt;...&lt;/svg&gt;
+    &lt;p class="strong"&gt;No Indent&lt;/p&gt;
     &lt;p&gt;Remove indenting from content.&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;</code></pre>
@@ -239,10 +199,6 @@
 
 
 <style lang="scss" scoped>
-  code.small {
-    font-size: 0.875rem;
-  }
-
   .example {
     margin-bottom: 1rem;
   }
