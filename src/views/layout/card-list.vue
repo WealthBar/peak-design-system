39000b78dbd1eba936baba45b2bb696328102734
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2020-08-24 15:48:10 -0700

    update examples

--- src/views/layout/card-list.vue
+++ src/views/layout/card-list.vue
@@ -8,18 +8,24 @@
       <div class="example">
         <div class="card">
           <ul class="card-list">
-            <li>Card in its simplest form</li>
-            <li>Margin is applied to the content using the <code>card-content</code> class.</li>
-            <li>This basic card is often used against a white background.</li>
+            <li>Here's <code>card-list</code> in its default form.</li>
+            <li>An unordered list of class <code>card-list</code> is nested insiade a <a href="card">card</a>.</li>
+            <li>If additional text styling or layout is needed, it can be applied to the list items.</li>
+          </ul>
+
+          <hr>
+
+          <ul class="card-list">
+            <li>A 'summary row' can be created by placing an <code>hr</code> between two <code>card-list</code>s.</li>
           </ul>
         </div>
       </div>
       <strong>Markup</strong>
       <pre><code class="language-markup">&lt;div class="card"&gt;
   &lt;ul class="card-list"&gt;
-    &lt;/li&gt;…&lt;/li&gt;
-    &lt;/li&gt;…&lt;/li&gt;
-    &lt;/li&gt;…&lt;/li&gt;
+    &lt;li&gt;…&lt;/li&gt;
+    &lt;li&gt;…&lt;/li&gt;
+    &lt;li&gt;…&lt;/li&gt;
   &lt;/ul&gt;
 &lt;/div&gt;</code></pre>
 
@@ -28,18 +34,18 @@
       <div cla ss="example">
         <div class="card">
           <ul class="card-list compact">
-            <li>Card in its simplest form</li>
-            <li>Margin is applied to the content using the <code>card-content</code> class.</li>
-            <li>This basic card is often used against a white background.</li>
+            <li>Here's <code>card-list</code> with a <code>compact</code> variant.</li>
+            <li>It bhaves the same as default card list but hase tighter spacing</li>
+            <li>If additional text styling or layout is needed, it can be applied to the list items.</li>
           </ul>
         </div>
       </div>
       <strong>Markup</strong>
       <pre><code class="language-markup">&lt;div class="card"&gt;
   &lt;ul class="card-list compact"&gt;
-    &lt;/li&gt;…&lt;/li&gt;
-    &lt;/li&gt;…&lt;/li&gt;
-    &lt;/li&gt;…&lt;/li&gt;
+    &lt;li&gt;…&lt;/li&gt;
+    &lt;li&gt;…&lt;/li&gt;
+    &lt;li&gt;…&lt;/li&gt;
   &lt;/ul&gt;
 &lt;/div&gt;</code></pre>
     </section>
