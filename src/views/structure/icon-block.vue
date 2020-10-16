1df15c956ada515b60e3f6e0de9d67b4b7d460dc
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2020-10-16 12:39:29 -0700

    cleanup per travis

--- src/views/structure/icon-block.vue
+++ src/views/structure/icon-block.vue
@@ -13,7 +13,7 @@
         <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
       </svg>
       <div>
-        <h6>Strong Title</h6>
+        <p class="strong">Strong Title</p>
         <p>The <code>.icon-block</code> pattern contains an <strong>image</strong> (<code>&lt;svg&gt;</code> or <code>&lt;img&gt;</code>) and a <strong>content block</strong> (<code>&lt;div&gt;</code>, <code>&lt;p&gt;,</code>, etc) For text. This format allows for an icon to be used like a bullet. Any nested content in the <strong>content block</strong> will be margined to offset the icon</p>
       </div>
     </article>
@@ -36,7 +36,6 @@
   &lt;/div&gt; &lt;!-- End Content Block --&gt;
 &lt;/div&gt;
 
-
 &lt;div class="icon-block"&gt;
   &lt;svg&gt;...&lt;/svg&gt; &lt;!-- Image --&gt;
   &lt;p&gt;this has no title.&lt;/p&gt; &lt;!-- Content Block --&gt;
@@ -50,7 +49,7 @@
         </svg>
         <p class="strong">Icon Block Content</p>
       </div>
-      <p class="icon-block-content">Sometimes design will require text to flow under the icon in small screens and align with text/content on large. In this scenario use <code>.icon-block-content</code> as a sibling (in the same parent) container to <code>.icon-block</code>.</p>
+      <p class="icon-block-content">Often body text will be required flow under the icon in small screens and align with text/content on large. In this scenario use <code>.icon-block-content</code> as a sibling (in the same parent) container to <code>.icon-block</code>.</p>
       <p class="icon-block-content">Margining is removed from the <code>.icon-block-content</code> mobile views and text will appear directly below the icon.</p>
       <hr />
       <div class="icon-block small">
@@ -78,8 +77,7 @@
 &lt;div class="icon-block small"&gt;
   &lt;svg&gt;...&lt;/svg&gt;
   &lt;p class="strong"&gt;This is Small&lt;/p&gt;
-&lt;/div&gt;
-&lt;p class="icon-block-content"&gt;The small class will produce smaller text and margin.&lt;/p&gt;</code></pre>
+&lt;/div&gt;</code></pre>
 
     <hr />
 
@@ -152,7 +150,6 @@
 &lt;/div&gt;</code></pre>
   </article>
 </template>
-
 
 <style lang="scss" scoped>
   .example {
