06a0c1f87859bf879e9a63246f322f5dd06d8b43
Andrew Pengelly <andrew.pengelly@wealthbar.com>
2020-10-20 10:10:22 -0700

    cleanup and improve documentation

--- src/views/structure/icon-block.vue
+++ src/views/structure/icon-block.vue
@@ -58,7 +58,21 @@
         </svg>
         <p class="strong">This is Small</p>
       </div>
-      <p class="icon-block-content">Use the class <code class="small">.small</code> to get a smaller 0.875rem (14px) font size and smaller bottom margin after <code>.icon-block</code>. This also works in conjunction with other variants such as<code>.icon-block-content</code> as is that case in this example.</p>
+      <p class="icon-block-content small">Use the class <code class="small">.small</code> to get a smaller 0.875rem (14px) font size and smaller bottom margin after <code>.icon-block</code>. This also works in conjunction with other variants such as<code>.icon-block-content</code> as is that case in this example.</p>
+    </article>
+    <hr />
+    <article class="icon-block">
+      <svg class="pos" viewBox="0 0 16 16" title="Information">
+        <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+      </svg>
+      <p>The Icon can be positive with a class of <code>.pos</code></p>
+    </article>
+    <hr />
+    <article class="icon-block">
+      <svg class="neg" viewBox="0 0 16 16" title="Information">
+        <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+      </svg>
+      <p>The Icon can be negative with a class of <code>.neg</code></p>
     </article>
 
     <strong>Markup</strong>
@@ -70,13 +84,60 @@
 
 &lt;!-- Sibling Icon Block Content --&gt;
 &lt;p class="icon-block-content"&gt;Margining is removed from the mobile views.&lt;/p&gt;
-&lt;p class="icon-block-content"&gt;you can include multiple siblings.&lt;/p&gt;
+&lt;p class="icon-block-content"&gt;You can include multiple siblings.&lt;/p&gt;
 
 &lt;hr&gt;
 
+&lt;!-- Small Variant Block --&gt;
 &lt;div class="icon-block small"&gt;
   &lt;svg&gt;...&lt;/svg&gt;
   &lt;p class="strong"&gt;This is Small&lt;/p&gt;
+&lt;/div&gt;
+&lt;p class="icon-block-content small"&gt;You can have small siblings here too.&lt;/p&gt;
+
+&lt;hr&gt;
+
+&lt;!-- Neg Icon--&gt;
+&lt;div class="icon-block"&gt;
+  &lt;svg class="pos"&gt;...&lt;/svg&gt;
+  &lt;p"&gt;positive icon colour&lt;/p&gt;
+&lt;/div&gt;
+
+&lt;hr&gt;
+
+&lt;!-- Pos Icon --&gt;
+&lt;div class="icon-block"&gt;
+  &lt;svg class="neg"&gt;...&lt;/svg&gt;
+  &lt;p class="strong"&gt;negative icon colour&lt;/p&gt;
+&lt;/div&gt;
+</code></pre>
+
+    <hr />
+
+    <h3>Icon Block Within a Notice</h3>
+    <p>Our Notices are designed to display a small amount of information. They work well with a single instance of <code>.icon-block</code>.</p>
+
+    <div class="notice example">
+      <div class="notice-content icon-block">
+        <svg class="pos" viewBox="0 0 16 16" title="Information">
+          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+        </svg>
+        <div>
+          <p class="strong">Notice Title</p>
+          <p>The Icon Block pattern can be nested inside layout styles such as <code>.notice</code>. You can apply the <code>.icon-block</code> directly to the <code>&lt;article&gt;</code> element or <code>.notice-content</code> class — or as a child of these containers.</p>
+        </div>
+      </div>
+    </div>
+
+    <strong>Markup</strong>
+    <pre><code class="language-markup">&lt;div class="notice"&gt;
+  &lt;div class="notice-content icon-block"&gt;
+    &lt;svg class="pos"&gt;...&lt;/svg&gt;
+    &lt;div&gt;
+      &lt;p class="strong"&gt;Notice Title&lt;/p&gt;
+      &lt;p&gt;Paragraphs and any other content under that title...&lt;/p&gt;
+    &lt;/div&gt;
+  &lt;/div&gt;
 &lt;/div&gt;</code></pre>
 
     <hr />
@@ -97,55 +158,53 @@
 
       <hr />
 
-      <article class="icon-block">
+      <article class="card-content icon-block">
         <svg viewBox="0 0 16 16" title="Information">
           <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
         </svg>
-        <p>This example shows how you can have multiple <code>.icon-block</code> containers and divide the card with an <code>&lt;hr&gt;</code>.</p>
+        <p>You can have multiple <code>.icon-block</code> containers and divide the card with an <code>&lt;hr&gt;</code>.</p>
       </article>
+
+      <hr />
+
+      <div class="card-content">
+        <div class="icon-block">
+          <svg viewBox="0 0 16 16" title="Information">
+            <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
+          </svg>
+          <p class="strong">Nested with Sibling Content</p>
+        </div>
+        <p class="icon-block-content">This example also nests the <code>.icon-block</code> inside a <code>.card-content</code> to allow for sibling <code>.icon-block-content</code> containers. (see change on mobile view)</p>
+      </div>
     </div>
 
     <strong>Markup</strong>
     <pre><code class="language-markup">&lt;div class="card shadow"&gt;
-  &lt;article class="icon-block"&gt;
+  &lt;header class="icon-block"&gt;
     &lt;svg&gt;...&lt;/svg&gt;
     &lt;div&gt;
       &lt;p class="strong"&gt;Default&lt;/p&gt;
       &lt;p&gt;Paragraphs and any other content under that title...&lt;/p&gt;
     &lt;/div&gt;
-  &lt;/article&gt;
+  &lt;/header&gt;
 
+  &lt;!-- Multi Icon Block Pattern--&gt;
   &lt;hr&gt;
 
   &lt;article class="icon-block"&gt;
     &lt;svg&gt;...&lt;/svg&gt;
     &lt;p&gt;Multiple icon block patterns in one card.&lt;/p&gt;
   &lt;/article&gt;
-&lt;/div&gt;</code></pre>
 
-    <h3>Icon Block Within a Notice</h3>
-    <p>Our Notices are designed to display a small amount of information. They work well with a single instance of <code>.icon-block</code>.</p>
+  &lt;hr&gt;
 
-    <div class="notice example">
-      <div class="notice-content icon-block">
-        <svg viewBox="0 0 16 16" title="Information">
-          <path d="M7,9.25 C7.41421356,9.25 7.75,9.58578644 7.75,10 C7.75,10.4142136 7.41421356,10.75 7,10.75 C6.58578644,10.75 6.25,10.4142136 6.25,10 C6.25,9.58578644 6.58578644,9.25 7,9.25 Z M7.5,3 L7.5,8 L6.5,8 L6.5,3 L7.5,3 Z M7,1 C10.3,1 13,3.7 13,7 C13,10.3 10.3,13 7,13 C3.7,13 1,10.3 1,7 C1,3.7 3.7,1 7,1 L7,1 Z M7,0 C3.15,0 0,3.15 0,7 C0,10.85 3.15,14 7,14 C10.85,14 14,10.85 14,7 C14,3.15 10.85,0 7,0 L7,0 Z" transform="matrix(1 0 0 -1 1 15)" />
-        </svg>
-        <div>
-          <p class="strong">Notice Title</p>
-          <p>The Icon Block pattern can be nested inside layout styles such as <code>.notice</code>. You can apply the <code>.icon-block</code> directly to the <code>&lt;article&gt;</code> element or <code>.notice-content</code> class — or as a child of these containers.</p>
-        </div>
-      </div>
-    </div>
-
-    <strong>Markup</strong>
-    <pre><code class="language-markup">&lt;div class="notice"&gt;
-  &lt;div class="notice-content icon-block"&gt;
-    &lt;svg&gt;...&lt;/svg&gt;
-    &lt;div&gt;
-      &lt;p class="strong"&gt;Notice Title&lt;/p&gt;
-      &lt;p&gt;Paragraphs and any other content under that title...&lt;/p&gt;
+  &lt;!-- Icon Block Content Pattern--&gt;
+  &lt;div class="card-content"&gt;
+    &lt;div class="icon-block"&gt;
+      &lt;svg&gt;...&lt;/svg&gt;
+      &lt;p class="strong"&gt;Nested with Sibling Content&lt;/p&gt;
     &lt;/div&gt;
+    &lt;p class="icon-block-content"&gt;Margining is removed from the mobile views.&lt;/p&gt;
   &lt;/div&gt;
 &lt;/div&gt;</code></pre>
   </article>
